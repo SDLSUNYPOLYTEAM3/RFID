179ebdfafae110b642e49d63c8f9c712be2dddb7
root <arbogas@sunypoly.edu>
2022-05-02 10:54:22 -0400

    updated gen2.py from local to RPI

--- gen2/gen2.py
+++ gen2/gen2.py
@@ -6,45 +6,55 @@
 import numpy as np
 
 #queur header input, this is a limited list for a specific query
-q_comand = np.array('i',[1,0,0,0])
+q_command = np.array([1,0,0,0])
 q_DR = 0
-q_M = np.array('i',[0,0])
+q_M = np.array([0,0])
 q_TRext = 0
-q_select = np.array('i',[0,0])
+q_select = np.array([0,0])
 q_target = 0
-q_Q = np.array('i',[0,0,0,0])
-q_crc5 = np.array('i',[,,,,])
+q_Q = np.array([0,0,0,0])
+q_crc5 = np.array([1,0,1,1,1])
 
-ack_comand = np.array('i',[0,1])
-ack_RC16 = 
+ack_command = np.array([0,1])
+#ack_RC16 = np.array('i',[1,0,1,1,1])
 
 #build header
-q_header = np.append(q_command,q_Dr,q_M,q_TRext,q_select,q_target,q_Q,q_crc5)
-ack_header = np.append(ack_command,ack_RC16)
+q_header = np.append(q_command,q_DR)
+q_header = np.append(q_header,q_M)
+q_header = np.append(q_header,q_TRext)
+q_header = np.append(q_header,q_select)
+q_header = np.append(q_header,q_target)
+q_header = np.append(q_header,q_Q)
+q_header = np.append(q_header,q_crc5)
+#ack_header = np.append(ack_command,ack_RC16)
+#print('header ',q_header)
 
-#encode the with PIE
 
-
-def pie_encode(header)
-    coded_header = np.array([])
-    zero = np.array([1,0])
-    one = np.array([1,1,1,0])    
+#endode the header with PIE
+coded_header = np.array([],int)
+zero = np.array([1,0])
+one = np.array([1,1,1,0])
+for x in q_header:
+    if x == 0:
+        coded_header = np.append(coded_header, zero)
+        #print(coded_header)
+    else:
+        coded_header = np.append(coded_header, one)
+        #print(coded_header)
     
-    for x in header;
-        if x == 0:
-            np.append(coded_header,zero)
-        else
-            np.append(coded_header, one)
-    
-    return coded_header
+#print('coded: ',coded_header)
     
 
 #add string of ones for continuouse wave, this lets the tag respond
-cw = np.ones(1000)
+cw = np.ones(1000) # this is a guese, need MATH
 np.append(coded_header,cw)
 
 
 #write to file, gnuradio will read from this file
+file_bits= bytearray(coded_header)
+newfile = open('query_bits.b','wb')
+newfile.write(file_bits)
+newfile.close()
 
 if__name__=="__main__":
     
